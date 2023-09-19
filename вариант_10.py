04013aa3f691eea58def96b92c5b4ce0256d3de4
Rozhkov Ivan <vanroz333@gmail.com>
2023-09-19 20:51:31 +0700

    ready

--- вариант_10.py
+++ вариант_10.py
@@ -1,4 +1,4 @@
 import math
 a = int(input())
 
-print(round(math.sin(math.pi / 2 + 3 * a) / (1 - math.sin(3 * a - math.pi)), 4))
+print("z1 =", round(math.sin(math.pi / 2 + 3 * a) / (1 - math.sin(3 * a - math.pi)), 4),"\n","z2 =", round(math.cos(5 / 4 * math.pi + 3 / 2 * a) / math.sin(5 / 4 * math.pi + 3 / 2 * a), 4))
